8a598dfea558f4bfeff168c34b8dd947f9e1255f
haruki yamaguchi <hrk1106hasu@gmail.com>
2022-12-23 15:20:02 +0900

    削除機能途中

--- components/AllTodos.js
+++ components/AllTodos.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import db from "../firebase";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query, doc, deleteDoc, setDoc } from "firebase/firestore";
 
 export const AllTodos = () => {
 
@@ -19,11 +19,27 @@ export const AllTodos = () => {
 
   },[])
 
+  const onClickDelete= async (index)=>{
+
+    const postData=collection(db,"todos");
+    const q=query(postData,orderBy("timestamp","desc"));
+
+    // リアルタイムでデータを取得
+    onSnapshot(q,(querySnapshot)=>{
+      const ids=querySnapshot.docs.map((todo)=>todo.id);
+      const targetId=ids[index];
+      deleteDoc(doc(db, "todos", `${targetId}`))
+    })
+  }
+
   return (
     <div>
-      {allTodos.map((allTodo)=>{
+      {allTodos.map((allTodo,index)=>{
         return (
-          <p key={allTodo.timestamp}>{allTodo.todo}</p>
+          <div key={allTodo.timestamp}>
+            <p>{allTodo.todo}</p>
+            <button onClick={()=>{onClickDelete(index)}}>削除</button>
+          </div>
         )
       })}
     </div>
--- components/InputTodo.js
+++ components/InputTodo.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react'
 import db from "../firebase";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 export const InputTodo = () => {
 
   const [todo,setTodo]=useState("");
 
-  const onClickAdd=(e)=>{
+  const onClickAdd=async(e)=>{
 
     e.preventDefault();
 
-    addDoc(collection(db,"todos"),{
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "todos"), {
       todo: todo,
       timestamp: serverTimestamp(),
-    })
+      isDelete: false,
+    });
+
+    // db.collection("todos").add({ id: null, todo: todo, timestamp: serverTimestamp() }).then(docRef => {
+    //   db.collection("todos").doc(docRef.id).update({
+    //       id: docRef.id
+    //   }).then(() => {
+    //       // success
+    //   }).catch(error => {
+    //   // error
+    //   })
+    //   }).catch(error => {
+    //       // error
+    //   })
 
     setTodo("");
   }
